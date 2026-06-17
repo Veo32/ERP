f94e247ec78621848502eb2dbee48b7331602ec7
Veo32 <veo1593570@gmail.com>
2026-06-18 01:15:46 +0200

    fix: redirect root to /desk

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/desk",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
